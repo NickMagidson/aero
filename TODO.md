d6352308de69c131094d2410fdfb94980a3691d4
Nick <nickmagi24@gmail.com>
2023-06-27 11:46:16 -0400

    Started todo list and slight code refactoring for readability

---
+++ TODO.md
@@ -0,0 +1,20 @@
+Aero:
+Air pollution app built with React, Next.js, Tailwind, and Node
+Pulls from Open Weather API. Will eventually have map funcitonality (with coord)
+
+Notes:
+CTRL + ALT + A to toggle Tawilwind Code readability
+
+
+1. Setup    
+  - ~~Setup and learn Next.js~~
+  - ~~Learn server components~~
+  - ~~API fetch testing for proof-of-concept~~
+  - ~~Create repo~~
+
+2. Mobile
+    - Build out and style Navbar component
+    - Fetch with OpenWeather air pollution API (start with one number)
+    - Design mobile component
+    - Conditionals to render a range of 'poor' to 'good. And final number
+    - Credit Karma style UI (probably a package for this)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,27 +32,18 @@ export default function Navbar({ fixed }) {
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  <span className="ml-2">Share</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
+                <a  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  <span className="ml-2">Tweet</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Pin</span>
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  <span className="ml-2">Pin</span>
                 </a>
               </li>
             </ul>
