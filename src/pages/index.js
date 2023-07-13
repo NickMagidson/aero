afe87f2227cf5dea5cc327e40d0da26ccde2ee08
Nick <nickmagi24@gmail.com>
2023-07-13 15:34:41 -0400

    Installed gauge chart component, commented for placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
 import AirData from './airData'
+import GaugeComponent from 'react-gauge-component'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,9 @@ export default function Home() {
       <main className={`min-h-screen flex flex-col items-center justify-between p-16 overscroll-none ${inter.className}`}>
         <div className="absolute z-5 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex p-9">
           <AirData />
+          {/* Gauge chart is here for placeholder and experimentation
+              Will most likely be implimenting in AirData */}
+          {/* <GaugeComponent id='gauge-component' /> */}
         </div>
       </main>
     </>
