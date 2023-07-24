a91189f663d63b2912fab98ec0648ed29c6feec7
Nick <nickmagi24@gmail.com>
2023-07-24 13:20:28 -0400

    Z index edit of Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 absolute w-full z-10">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 absolute w-full z-20">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 z-10 relative">
           <a href="https://flowbite.com/" className="flex items-center">
               <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
