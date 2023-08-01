f85fabed47d2336114f80042641e6e397f53e604
Nick Magidson <Nickmagi24@gmail.com>
2023-08-01 16:53:11 -0400

    Added SearchBar component, more responsivness, and data container positioning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
       <nav className="bg-white border-gray-200 dark:bg-gray-900 absolute w-full z-20">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 z-10 relative">
           <a href="https://flowbite.com/" className="flex items-center">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+              <img src="https://img.icons8.com/color/48/000000/air-element.png"  className="h-10 mr-3 w-10" alt="Flowbite Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AERO</span>
           </a>
           <button onClick={handleMenuToggle} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={isMenuOpen}>
