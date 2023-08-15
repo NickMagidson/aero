9907630ade36b337e86d46722f6654c7f46f14e8
Nick Magidson <Nickmagi24@gmail.com>
2023-08-15 14:21:28 -0400

    Fixed typed error in AirDataGrid

--- src/components/AirDataGrid.tsx
+++ src/components/AirDataGrid.tsx
@@ -9,13 +9,13 @@ interface AirDataGridProps {
   value: number;
 }
 
-const AirDataGrid: React.FC<AirDataGridProps> = ({ data  }) => {
+const AirDataGrid: React.FC<AirDataGridProps> = ({ data }) => {
   return (
     <div id='data-grid' className={`text-lg text-center p-5 flex flex-col ${inter.className} grid grid-cols-3 grid-rows-3 gap-3 rounded-2xl bg-black bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100 `}> 
     {Object.entries(data.components).map(([key, value]) => (
       <div key={key} className='flex flex-col justify-center items-start w-100 border-0 rounded-md p-1 bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-none bg-opacity-20 border-gray-100 shadow-[0_8px_30px_rgb(0,0,0,0.12)]'>
         <h3 className='text-left font-medium text-sm text-white' key={key}>
-          <CountUp end={value} duration={6} decimals={2} decimal="." />
+          <CountUp end={value as number} duration={6} decimals={2} decimal="." />
           <br/> 
           <span className="text-sm font-light text-gray-200">{key}:</span>
         </h3>
