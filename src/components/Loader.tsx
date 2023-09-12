c8faab03ea5077ca66826d716395b59b7cc4ab63
Nick Magidson <Nickmagi24@gmail.com>
2023-09-12 12:41:34 -0400

    Centering loaders

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,15 +3,17 @@ import { Circles } from 'react-loader-spinner'
 const Loader: React.FC = () => {
   return (
     <>
-      <Circles
-        height="80"
-        width="80"
-        color="#3b7fd9"
-        ariaLabel="circles-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        />
+      <div className='flex justify-center w-full'>
+        <Circles
+          height="80"
+          width="80"
+          color="#3b7fd9"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          />
+      </div>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,6 @@ const Home: React.FC<HomeProps> = () => {
       <Head>
         <title>Aero | Air Pollution Dashboard</title>
         <link rel='icon' href='/airLogo.png' sizes='any' />
-        {/* <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' /> */}
       </Head>
       <main className={`min-h-screen flex flex-col items-center justify-between p-16 overscroll-none z-10 ${inter.className}`}>
         <div id='data-container'  className="flex flex-col justify-center z-10 h-auto w-72 mt-auto mb-auto md:w-auto lg:justify-start 2xl:mr-auto max-w-2xl 2xl:max-w-[288px] 2xl:min-w-[288px] ">
