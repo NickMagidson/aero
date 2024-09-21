f37833959cb5b7d0a01f1b65e9ce6ebae0dccd1d
Nick <nickmagi24@gmail.com>
2024-09-21 19:40:06 -0400

    Commented out spinner animation

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,11 +4,11 @@ const Loader: React.FC = () => {
   return (
     <>
       <div className='flex flex-col justify-center w-full'>
-        <RingLoader
+        {/* <RingLoader
           color="#36d7b7"
           loading
           size={300}
-        />
+        /> */}
         <h1 className='m-auto text-3xl bg-gradient-to-r from-sky-600 to-cyan-500 bg-clip-text text-transparent'>Loading...</h1>
       </div>
     </>
