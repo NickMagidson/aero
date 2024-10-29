f2b9778887ee3cbd0062b2ffdfd20f5f8783a1d8
Nick <nickmagi24@gmail.com>
2024-10-29 19:17:10 -0400

    Added meta title and commented map styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from 'next/head';
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Aero</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
