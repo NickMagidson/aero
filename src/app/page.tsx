dce024a27dd05f5e6a7be77bf13b6902ea09471c
Nick <nickmagi24@gmail.com>
2024-10-29 20:26:15 -0400

    Drag ability working, psuedo code for polishing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import SearchBar from './components/SearchBar';
+import Draggable from 'react-draggable';
 
 const Map: any = dynamic(
   () => import('@/app/components/Map/Map'),
@@ -34,6 +35,13 @@ export default function Home() {
   };
 
 
+  // Draggable Data Component
+  // 1. Make body heigh 100vh
+  // 2. Position the parent container, overall height will go out of body bounds
+  // 3. The drag div itself will assume a fixed height (for needed data/information)
+  // 4. Should function at bare bones. Polish as needed
+  // 5. Next: inner scrolling, style handle, start design
+
 
   return (
 
@@ -42,6 +50,40 @@ export default function Home() {
         <SearchBar setLat={handleSearchLat} setLon={handleSearchLon}  />
       </div>
       <Map accessToken={MAPBOX_API_KEY} lat={lat} lon={lon} /> 
+
+
+
+
+
+      {/* Parent container for data component */}
+      <div className='absolute overflow-visible bottom-0 border-2 border-red-700 w-full h-80 z-10'>
+
+        <Draggable 
+          axis="y" 
+          bounds="parent"
+          handle=".dragbar"
+          >
+          <div className="glassmorphism w-11/12 h-auto mx-auto">
+            <strong className="cursor"><div className='dragbar w-1/2 p-4 border-2 mx-auto border-red-700 text-center cursor'>Drag here</div></strong>
+              <p>This is some placeholder test.</p>
+          </div>
+        </Draggable>
+
+      </div>
+
+      {/* <Draggable
+        axis="y"
+        handle=".handle"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        >
+        <div>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
+        </div>
+      </Draggable> */}
   
       {/* Search Bar */}
       {/* Info Div */}
